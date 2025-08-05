538c34a9bc1851f9b1d43fe6a29f98d4ae7b591f
mfcss <mf@csselectronics.com>
2025-08-05 20:22:08 +0200

    no message

--- mdftoparquet-backlog/prepare_container_release.bat
+++ mdftoparquet-backlog/prepare_container_release.bat
@@ -5,10 +5,13 @@ echo ---------------------------------------------
 :: Create release directory if it doesn't exist
 if not exist "..\release\backlog-processor-container" mkdir "..\release\backlog-processor-container"
 
-:: Copy requirements files
-echo Copying requirements files...
-copy "container-root\requirements_azure.txt" "..\release\backlog-processor-container\"
-copy "container-root\requirements_amazon.txt" "..\release\backlog-processor-container\"
+:: Copy requirements file
+echo Copying requirements file...
+copy "container-root\requirements.txt" "..\release\backlog-processor-container\"
+
+:: Copy Dockerfile
+echo Copying Dockerfile...
+copy "container-root\Dockerfile" "..\release\backlog-processor-container\"
 
 :: Copy decoder executable
 echo Copying decoder executable...
